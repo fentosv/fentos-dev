685a3018b4d309c9dc543067ae585516fb930bf0
fentosv <fentos.dev@gmail.com>
2022-11-20 17:43:30 +0100

    Update to Next13

--- next.config.js
+++ next.config.js
@@ -1,21 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
-  experimental: {
-    images: {
-      allowFutureImage: true,
-    },
-  },
   i18n: {
     locales: ['en', 'es'],
-    defaultLocale: 'es'
-  }
+    defaultLocale: 'es',
+  },
 }
 
 module.exports = nextConfig
-
-
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,108 +1,81 @@
 import React from 'react'
 import Link from 'next/link'
-// import Image from 'next/image';
-import Image from 'next/future/image'
-import { useRouter } from 'next/router';
+import Image from 'next/image'
+import { useRouter } from 'next/router'
 import classnames from 'classnames-creator'
-import Settings from '@components/Settings';
+import Settings from '@components/Settings'
 
-import logo from '@public/logo.svg';
+import logo from '@public/logo.svg'
 import s from './Navbar.module.scss'
 
 interface Props {
-    navbarClass?: string
+  navbarClass?: string
 }
 
 const navbarContent = {
-    "en": {
-        about: {
-            title: "About"
-        },
-        test: {
-            title: "Test"
-        }
+  en: {
+    about: {
+      title: 'About',
     },
-    "es": {
-        about: {
-            title: "Información"
-        },
-        test: {
-            title: "Prueba"
-        }
-    }
-};
-
-
-function Navbar({ navbarClass }: Props) {
-
-    const router = useRouter()
-    const { pathname, asPath, query, locale, locales, defaultLocale } = router
-
-    const t = locale === 'es' ? 'es' : 'en';
-
-    const { about, test } = navbarContent[t]
-
-    return (
-        <>
-            <header
-                className={
-                    classnames(
-                        s.navbar,
-                        {
-                            [s[navbarClass ?? '']]: navbarClass
-                        }
-                    )}
-            >
-                <div className={s.navbar_container}>
-
-                    <Link href='/'>
-                        <a>
-                            <Image
-                                className={s.navbar_icon_img}
-                                src={logo}
-                                alt='Fentos logo'
-                                title='Fentos'
-                                priority
-                            />
-                        </a>
-                    </Link>
-
-                    <Link href='/about'>
-                        {/* Use bracket notation for BEM implementation */}
-                        <a
-                            title={about.title}
-                            className={
-                                classnames(
-                                    s.navlink,
-                                    {
-                                        [s['navlink--active']]: router.pathname == '/about'
-                                    }
-                                )}
-                        >
-                            {about.title}
-                        </a>
-                    </Link>
-                    <Link href='/test'>
-                        <a
-                            className={
-                                classnames(
-                                    s.navlink,
-                                    {
-                                        [s['navlink--active']]: router.pathname == '/test'
-                                    }
-                                )}
-                        >
-                            {test.title}
-                        </a>
-                    </Link>
-
-
-                    <Settings />
-
-                </div >
-            </header >
-        </>
-    );
+    test: {
+      title: 'Test',
+    },
+  },
+  es: {
+    about: {
+      title: 'Información',
+    },
+    test: {
+      title: 'Prueba',
+    },
+  },
 }
 
-export default Navbar;
+function Navbar({ navbarClass }: Props) {
+  const router = useRouter()
+  const { pathname, asPath, query, locale, locales, defaultLocale } = router
+
+  const t = locale === 'es' ? 'es' : 'en'
+
+  const { about, test } = navbarContent[t]
+
+  return (
+    <>
+      <header
+        className={classnames(s.navbar, {
+          [s[navbarClass ?? '']]: navbarClass,
+        })}
+      >
+        <div className={s.navbar_container}>
+          <Link href='/'>
+            <Image className={s.navbar_icon_img} src={logo} alt='Fentos logo' title='Fentos' priority />
+          </Link>
+
+          <Link
+            href='/about'
+            title={about.title}
+            className={classnames(s.navlink, {
+              [s['navlink--active']]: router.pathname == '/about',
+            })}
+          >
+            {/* Use bracket notation for BEM implementation */}
+
+            {about.title}
+          </Link>
+          <Link
+            href='/test'
+            className={classnames(s.navlink, {
+              [s['navlink--active']]: router.pathname == '/test',
+            })}
+          >
+            {test.title}
+          </Link>
+
+          <Settings />
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Navbar
